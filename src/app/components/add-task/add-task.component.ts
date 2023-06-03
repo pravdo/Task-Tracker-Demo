82acf412f22ecd2c6d8d6ee844fedb2c1cd16281
Pravdomir Kostov <pravdomir1998@gmail.com>
2023-06-03 16:35:36 +0300

    base app logic

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,43 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Task } from '../../Task';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text?: string;
+  day: string | any;
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  onSubmit() {
+    if (!this.text) {
+      alert('You shoud add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
